ee6cfc36f38c245b89aaf90a4168d8c9af584f8b
Yuri Wingester <yuri2jwingester@gmail.com>
2016-08-25 23:34:36 +0200

    Projecto Inicial - Config com Spring

---
+++ src/main/java/co/mz/caelum/romanos/controllers/CoreController.java
@@ -0,0 +1,16 @@
+package co.mz.caelum.romanos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CoreController 
+{
+	@RequestMapping("/teste")
+	public String teste()
+	{
+		return "index";
+	}
+	
+	
+}
